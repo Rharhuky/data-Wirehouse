7f49f2cd42c3fcf8f5f901dd3739a3bd11f9b659
= <=>
2023-10-29 10:57:04 -0300

    Esqueleto do projeto

---
+++ src/test/java/com/datawareHouse/DatawareHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawareHouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawareHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
