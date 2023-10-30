fbb8c5f4c5831e052228e31a900ff37b4e791962
= <=>
2023-10-30 16:23:43 -0300

    Excluindo ProdutoRepository p/ pull

--- src/main/java/com/datawareHouse/repository/ProdutoRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.datawareHouse.repository;
-//
-//import com.datawareHouse.model.Produto;
-//import org.springframework.data.cassandra.repository.CassandraRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface ProdutoRepository extends CassandraRepository<Produto, Long> {
-//}
